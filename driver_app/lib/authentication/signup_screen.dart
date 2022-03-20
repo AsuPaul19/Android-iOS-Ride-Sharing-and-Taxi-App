71e3dab40e1e4a174ee59d9d0fd59ccc4f97d26b
Paul Asu <asupaullane@yahoo.com>
2022-03-19 22:24:51 -0700

    created driver app & added home, signup, main, earnings, etc screens

---
+++ driver_app/lib/authentication/signup_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget
+{
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+
+
+class _SignUpScreenState extends State<SignUpScreen>
+{
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController phoneTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+
+              const SizedBox(height: 10,),
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("images/logo1.png"),
+              ),
+
+              const SizedBox(height: 10,),
+
+              const Text(
+                "Register as a Driver",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              TextField(
+                controller: nameTextEditingController,
+                style: const TextStyle(
+                  color: Colors.grey
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Name",
+                  hintText: "Name",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+              TextField(
+                controller: emailTextEditingController,
+                style: const TextStyle(
+                    color: Colors.grey
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  hintText: "Email",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+              TextField(
+                controller: phoneTextEditingController,
+                style: const TextStyle(
+                    color: Colors.grey
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Phone",
+                  hintText: "Phone",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+              TextField(
+                controller: passwordTextEditingController,
+                style: const TextStyle(
+                    color: Colors.grey
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                  hintText: "Password",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
